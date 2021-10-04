6193a5c8ca34e89001d8ed6ea06b14a8f8cc66de
Huybnm2008110178 <minhhuybui559@gmail.com>
2021-10-04 20:36:31 +0700

    thêm class cow + testdrive

---
+++ src/vn/edu/giadinh/tuan03/Cow.java
@@ -0,0 +1,18 @@
+package vn.edu.giadinh.tuan03;
+
+public class Cow {
+
+    double weight;
+
+    //HÀM TẠO
+    //developers tạo ra KHÔNG DỂ java compiler tạo ra
+    
+    Cow(){//HÀM TẠO MẶC ĐỊNH DO developer tạo ra 
+
+    }
+
+    Cow(int i){
+        weight = i;
+    }
+    
+}
