277816092aa81521df5d530cd1eab951294c0bf1
Huybnm2008110178 <minhhuybui559@gmail.com>
2021-10-05 16:29:08 +0700

    Minh họa khai báo và khởi tạo mảng

---
+++ src/vn/edu/giadinh/tuan04/CTMinhHoaMang.java
@@ -0,0 +1,30 @@
+package vn.edu.giadinh.tuan04;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class CTMinhHoaMang {
+    public static void main (String[] args){
+        //cú pháp khai báo mảng
+        int[] mangSoNguyen; //chưa chỉ định số phân tử
+        
+        mangSoNguyen = new int[5]; //=> mảng trong java là 1 kiểu tham chiếu đối tượng
+
+        double mangSoThuc[] = new double[10];
+        
+        //trước khi dùng thì phải xác định số phân tử của mảng
+        mangSoNguyen[0] = 2;
+        
+        //KHAI BÁO CÓ KHỞI TẠO GIÁ TRỊ CHO MẢNG
+        double diemMonHoc[] = new double[]{10.5, 5.5, 3.0, 4.5};
+        System.out.println("Mảng điểm môn học: " + Arrays.toString(diemMonHoc));
+        diemMonHoc[0] = diemMonHoc[1];
+        //5.5, 5.5, 3.0, 4.5
+
+        System.out.println("Mảng điểm môn học: " + Arrays.toString(diemMonHoc));
+        
+        
+        //CÁCH 2
+        String dsTenSinhVien[] = {"Tèo", "Tý", "Tửng"};    
+    }
+}
