e5859172262d2bc2618e69bf88483b38f3ccecde
Huybnm2008110178 <minhhuybui559@gmail.com>
2021-10-08 14:48:58 +0700

    bài tập

--- src/vn/edu/giadinh/tuan05/CTMinhHoaArrayList.java
+++ src/vn/edu/giadinh/tuan05/CTMinhHoaArrayList.java
@@ -3,18 +3,34 @@ package vn.edu.giadinh.tuan05;
 import java.util.ArrayList;
 import java.util.List;
 
+import vn.edu.giadinh.tuan04.Student;
+import vn.edu.giadinh.tuan04.dog;
+
 public class CTMinhHoaArrayList {
     public static void main(String[] args){
          
         ArrayList a = new ArrayList();
-        
-        List bienTCArrayList = new ArrayList();
-        
         a.add("Cường");
-        a.add(true);
         a.add(1);
-        a.add(2.5);
-        Integer x = (Integer)a.get(2);
+        a.add(new dog(22, "Vàng", "Fido"));
+
+        dog dog = (dog) a.get(2);
+        System.out.println("Dog name: "+ dog.name + " color " + dog.color);
+
+        a.add(1.5);
+       
+        System.out.println(a.get(0));
+
+
+
+        List bienTCArrayList = new ArrayList();
+
+        bienTCArrayList.add(new Student("Tèo", 18));
+        
+        //Lấy 1 phần tử từ list
+        Student student = (Student) bienTCArrayList.get(0);
+
+        System.out.println(bienTCArrayList);
 
     }
     
