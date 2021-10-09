a4ef292ddbc2a679e4cfe725d35087ba16ca1337
Huybnm2008110178 <minhhuybui559@gmail.com>
2021-10-09 16:18:54 +0700

    bài tập

---
+++ src/com/huybnm2008110178/tuan01/Lab3.java
@@ -0,0 +1,16 @@
+package com.huybnm2008110178.tuan01;
+
+import java.util.Scanner;
+
+public class Lab3 {
+    public static void main (String[] args){
+        int Canh;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập cạnh: ");
+        Canh = scanner.nextInt();
+
+        int TheTich = Canh*Canh*Canh;
+        System.out.println("Thể tích của khối chữ nhật: " + TheTich);
+    }
+    
+}
---
+++ src/com/huybnm2008110178/tuan01/Lab4.java
@@ -0,0 +1,23 @@
+package com.huybnm2008110178.tuan01;
+
+import java.util.Scanner;
+
+public class Lab4 {
+    public static void main (String[] args){
+        double a,b,c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập hệ số a: ");
+        a = scanner.nextDouble();
+        System.out.println("Nhập hệ số b: ");
+        b = scanner.nextDouble();
+        System.out.println("Nhập hệ số c: ");
+        c = scanner.nextDouble();
+        
+        Double Delta = Math.pow(b,2)-4*a*c;
+        Double canDelta = Math.sqrt(Delta);
+
+        System.out.println("Delta: " + Delta);
+        System.out.println("căn của Delta: " + canDelta);
+    }
+    
+}
