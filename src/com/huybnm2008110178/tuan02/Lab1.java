46bdbb416fd3ec6a31ae351d5c392ef4d0c9a8e1
Huybnm2008110178 <minhhuybui559@gmail.com>
2021-10-09 16:21:21 +0700

    bài tập

---
+++ src/com/huybnm2008110178/tuan02/Lab1.java
@@ -0,0 +1,31 @@
+package com.huybnm2008110178.tuan02;
+
+import java.util.Scanner;
+
+public class Lab1 {
+    public static void main(String[] args){
+        Double a,b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập hệ số a: ");
+        a = scanner.nextDouble();
+        System.out.println("Nhập hệ số b: ");
+        b = scanner.nextDouble();
+
+        if(a == 0){
+            if(b == 0){
+
+                System.out.println("Phương trình có vô số nghiệm ");
+
+            }
+            else{
+
+                System.out.println("Phương trình vô nghiệm ");
+            }
+
+        }
+        else{
+
+            System.out.println("Phương trình có nghiệm: " + (-b/a));
+        }
+    }
+}
