73179c5268996dd594bd54e6a65f02e70e0f92b6
Huybnm2008110178 <minhhuybui559@gmail.com>
2021-10-18 10:14:44 +0700

    bài tập

---
+++ src/vn/edu/giadinh/tuan07/Node.java
@@ -0,0 +1,22 @@
+package vn.edu.giadinh.tuan07;
+
+public class Node {
+    //Nội dung
+    int data;
+
+    //Liên kết đến nút tiếp theo
+    //Tham chiếu
+    Node next; //Biến tham chiếu kiểu Node
+
+    public Node(){}
+
+    public Node(int d){
+        data = d;
+    }
+
+    public Node(int d, Node n){
+        data = d;
+        next = n;
+    }
+    
+}
