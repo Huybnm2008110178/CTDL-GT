34ec00d8a913beab883192d0413851698dd033ad
Huybnm2008110178 <minhhuybui559@gmail.com>
2021-10-18 10:14:37 +0700

    bài tập

---
+++ src/vn/edu/giadinh/tuan07/MyLinkedList.java
@@ -0,0 +1,27 @@
+package vn.edu.giadinh.tuan07;
+
+public class MyLinkedList {
+
+    Node head = null;
+    Node tail = null;
+    
+
+    public MyLinkedList(){}
+
+
+    void add(){
+
+    }
+
+    void remove(){
+
+    }
+
+    void print(){
+
+    }
+
+    void find (){
+
+    }
+}
